a1529e6c7b72304a19a66f6fa951208351c52e69
TaraWeidner <taratalnenetz@gmail.com>
2026-07-28 12:42:34 -0700

    Connect Firebase Realtime Database

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig={
   apiKey:"AIzaSyBVr0hDZaYFaHbXd4eqXflHvWREG2KPq6Q",
   authDomain:"inclusive-health-open-house.firebaseapp.com",
-  databaseURL:"REPLACE_WITH_FIREBASE_DATABASE_URL",
+  databaseURL:"https://inclusive-health-open-house-default-rtdb.firebaseio.com/",
   projectId:"inclusive-health-open-house",
   appId:"1:101856084348:web:c82969c6a3c8821540e4f9"
 };
